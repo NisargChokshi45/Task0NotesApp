b40111d6127b1945aea6459563ed8dfb0eec5abd
Nisarg Chokshi <nisarg4000@gmail.com>
2021-02-13 19:42:20 +0530

    Lesson 3 - Importing NPM Modules

---
+++ lesson3.js
@@ -0,0 +1,15 @@
+const validator = require("validator");
+
+// console.log(validator.contains("ENNavn", "en", { ignoreCase: true }));
+
+// console.log(validator.equals("wonderFul", "Wonderful"));
+
+// console.log(validator.isAlpha("pro_Star"));
+
+// console.log(validator.isJSON(`{"name": "john doe", "mobile": "9876543210"}`));
+
+// console.log(validator.isStrongPassword("!Lov3MyPiano"));
+
+// console.log(validator.isURL("https://www.npmjs.com/package/validator"));
+
+console.log(validator.isEmail("press@npmjs.com"));
