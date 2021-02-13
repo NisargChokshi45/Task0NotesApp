48612068833584951afe6f32733ab4ae76d25670
Nisarg Chokshi <nisarg4000@gmail.com>
2021-02-13 20:32:28 +0530

    Lesson 4 - Importing Global NPM Modules and Nodemon

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// CHECKPOINT
+// Challenge 3 : Use the Chalk library in your Project
+
 const { showName } = require("./lesson2");
 
 showName("John Doe");
---
+++ lesson4.js
@@ -0,0 +1,9 @@
+const chalk = require("chalk");
+
+console.log(chalk.inverse.bold("Hello World !"));
+console.log(chalk.green.bold.inverse("Hello World !"));
+console.log(chalk.black.bgYellow.underline.bold("Hello World !"));
+console.log(chalk.magenta.bold("Hello World !"));
+console.log(chalk.cyan.bold("Hello World !"));
+console.log(chalk.bgRed.white.bold("Hello World !"));
+console.log(chalk.bgCyan.gray.bold("Hello World !"));
